250ec2191601cccbb9e6ac61527485bed5dfc4b4
Anthony Chang <awchang12@gmail.com>
2018-05-03 00:28:08 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,7 +31,8 @@ end
 def initial_round
   # code #initial_round here
   new_card_total = deal_card + deal_card
-  return display_card new_card_total
+  display_card new_card_total
+  return new_card_total
   
 end
 
